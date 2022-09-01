011fbe00962f8101b2d598487c3da28013cc75f1
melspence <melvillespence@hamnavoesolutions.com>
2022-09-01 21:59:10 +0100

    Section 5 complete

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
+  <main-nav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav/MainNav.vue";
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,16 @@
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucumber", "Radish"];
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ["Vue", "hTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
 
-console.log([...fruits, ...vegetables]);
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+developer.doubleSalary();
+console.log(developer.salary);
+console.log(developer.lookingForWork);
